a40a659faffba1c3be1141789043c0b99e19f3b9
yilmazkaraagac <karaagacylmaz@hotmail.com>
2022-01-05 01:33:46 +0300

    second test

--- GitTestiOSProject/ViewController.swift
+++ GitTestiOSProject/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("GitHub Test")
-        
+        print("GitHub Test2")
     }
 
 
