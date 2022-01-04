512a142b3fd3287c178fb9baad34fdb7b4205508
yilmazkaraagac <karaagacylmaz@hotmail.com>
2022-01-05 01:19:24 +0300

    first commit

---
+++ GitTestiOSProject/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GitTestiOSProject
+//
+//  Created by Yılmaz Karaağaç on 1/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("GitHub Test")
+        
+    }
+
+
+}
+
